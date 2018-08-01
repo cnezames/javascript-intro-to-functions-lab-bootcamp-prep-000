c454e4d02fc557f466b182227fea4327717e8ff9
Cynthia Nezames <cnezames@gmail.com>
2018-08-01 14:54:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === whisper(string)) {
-    console.log("I can't hear you!")
+    console.log("I can't hear you!");
   } else if (string === shout(string)) {
-    console.log("YES INDEED!")
+    console.log("YES INDEED!");
   } else if (string === "I love you, Grandma.") {
     console.log("I love you, too.")
   }
