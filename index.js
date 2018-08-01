2074a0d7972fab1088ac626d61d56e9760bbb78e
Cynthia Nezames <cnezames@gmail.com>
2018-08-01 14:49:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string = Whisper(string)) {
+  if (string = Shout(string)) {
       console.log('I can\'t hear you!')
   }
   else if (string = Whisper(string)) {
-      console.log(Shout(string))
+      console.log('YES INDEED')
   }
-  else {console.log('I love you, too.')
+  else if (string = "I love you, Grandma.") {console.log('I love you, too.')
 }
