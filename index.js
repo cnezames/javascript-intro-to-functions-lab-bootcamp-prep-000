70308216f6f04ef7cdee051d4f0b83987d1d2abb
Cynthia Nezames <cnezames@gmail.com>
2018-08-01 14:47:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  if (string = Whisper(string)) {
+      console.log('I can\'t hear you!')
+  }
+  else if (string = Whisper(string)) {
+      console.log(Shout(string))
+  }
+  else {console.log('I love you, too.')
+}
